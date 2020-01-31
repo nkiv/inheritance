68f29dda716d10faf54d382e7cd9043b4b97a3a7
nkiv <no.kivett@gmail.com>
2020-01-30 22:30:50 -0700

    Worked on Account

--- CurrentAccount.java
+++ CurrentAccount.java
@@ -1,4 +1,29 @@
 public class CurrentAccount extends Account{
 
+    public double bal;
+
+    public CurrentAccount(int a, double bal){
+        super(a);
+        this.bal = 0.0;
+    }
+
+    public void withdraw(double sum){
+	if (sum>0){
+	    bal-=sum;    
+    }else if(sum>bal){
+        System.err.println("You cannot withdraw more than account balance!");
+    }
+	    System.err.println("Account.withdraw(...): "
+			       +"cannot withdraw negative amount.");    
+    }
+    
+
+    public void Overdrawn(){
+        if(bal<-100){
+            System.err.println("You cannot overdraw your account over $100!");
+        }else{
+            System.out.println("==================");
+        }
+    }
 
 }
--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,4 +1,25 @@
-public class SavingsAccount extends Account{
+public class SavingsAccount extends Account {
+    private double bal;
+    private double interest = 1.01;
 
+    public SavingsAccount( int a, double bal,  double interest) {
+        super(a);
+        this.interest = interest;
+        this.bal = (0.0);
+    }
+
+    public void withdraw(double sum){
+        if (sum>0){
+            bal-=sum;    
+        }else if(sum>bal){
+            System.err.println("You cannot withdraw more than account balance!");
+        }
+            System.err.println("Account.withdraw(...): "
+                       +"cannot withdraw negative amount.");    
+        }
+
+    public double accurInterest(){
+        return bal = bal * interest;
+    }
 
 }
